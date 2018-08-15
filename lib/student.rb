f2b2595cfb1b27a8dc43bc5b1c40bcf310deb0d0
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-15 21:17:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,9 +44,7 @@ class Student
       WHERE grade <= 11 
     SQL
     
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
+    DB[:conn].execute(sql)
   end 
   
   def save
