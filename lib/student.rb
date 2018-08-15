8c9232746444b248f812647de0fdeed4f685abb7
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-15 21:27:12 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -76,6 +76,10 @@ class Student
   end 
   
   def self.first_X_students_in_grade_10(num)
-    
+    sql = <<-SQL
+      SELECT * FROM students 
+      WHERE grade = 10 
+      
+      SQL
   end 
 end
