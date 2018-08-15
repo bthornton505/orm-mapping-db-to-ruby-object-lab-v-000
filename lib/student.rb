41f9f9443c379599f53b68c06d6af057d35f2597
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-15 21:16:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,13 +38,13 @@ class Student
     DB[:conn].execute(sql)
   end 
   
-  def self.students_below_12th_grade(name)
+  def self.students_below_12th_grade
     sql = <<-SQL
       SELECT students.name FROM students 
       WHERE grade <= 11 
     SQL
     
-    DB[:conn].execute(sql, name).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
   end 
